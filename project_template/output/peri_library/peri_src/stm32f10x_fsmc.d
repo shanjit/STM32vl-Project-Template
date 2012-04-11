72bc270140f82125768cbe2a172af9cef8075113
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2012-04-12 03:46:55 +0530

    todo committed

---
+++ project_template/output/peri_library/peri_src/stm32f10x_fsmc.d
@@ -0,0 +1,33 @@
+peri_library/peri_src/stm32f10x_fsmc.d \
+ peri_library/peri_src/stm32f10x_fsmc.o: \
+ ../peri_library/peri_src/stm32f10x_fsmc.c \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_fsmc.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/src/stm32f10x.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/src/core_cm3.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/src/system_stm32f10x.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/src/stm32f10x_conf.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_exti.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_gpio.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_rcc.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_usart.h \
+ /home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/misc.h
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_fsmc.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/src/stm32f10x.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/src/core_cm3.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/src/system_stm32f10x.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/src/stm32f10x_conf.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_exti.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_gpio.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_rcc.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/stm32f10x_usart.h:
+
+/home/cuil/workspace/arm/stm32_linux/gpio/peri_library/peri_inc/misc.h:
